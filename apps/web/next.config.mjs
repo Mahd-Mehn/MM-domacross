a32d277818ebb278a16663c832563e4171e3ed87
manny-uncharted <omoebun52@gmail.com>
2025-10-08 16:34:19 +0100

    fixed

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -12,6 +18,13 @@ const nextConfig = {
     fetches: {
       fullUrl: true,
     },
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
   },
 };
 export default nextConfig;
